15b5881d68e0c999728cb936b3ef1fd75415f6e5
Paula McKinley <pmknyc@optimum.net>
2018-10-11 21:31:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,16 @@
 require 'pry'
+# deli arrays: lists of customers at different delis
 carnegie_deli = ["Ann", "Betty","Cathy","Dina","Ella","Fran","Gabby", "Helen"]
 katz_deli = []
 
-# Line:  customers in order of their place in line
+# line method:  customers in order of their place in line
 def line(deli)
   if deli.empty?
     puts "The line is currently empty."
   else
     line_order = deli.map.with_index(1) do |name, index|
       "#{index}. #{name}"
-      end.join(" ")
+      end.join(" "
     puts "The line is currently: #{line_order}"
   end
 end
@@ -24,8 +25,15 @@ def take_a_number(array, name)
   array
 end
 
- # now_serving puts  next person in line and remove them from front of array.
+ # now_serving finds first person in line
+ # puts notice "Currently serving #{first_person)." 
+ # and removes first person from front of array.
  # If nobody in line, puts out "There is nobody waiting to be served!"
-def now_serving (name)
-
+def now_serving (deli)
+  if deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deli.shift}"
+    deli.shift
+  end
 end
