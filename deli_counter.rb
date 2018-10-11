9f6bd0eca4736171eeadf215476893cc0405a902
Paula McKinley <pmknyc@optimum.net>
2018-10-11 21:10:35 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,18 +7,21 @@ def line(deli)
   if deli.empty?
     puts "The line is currently empty."
   else
-    line_order = deli.map.with_index do |name, index|
-      "#{index + 1}. #{name}"
+    line_order = deli.map.with_index(1) do |name, index|
+      "#{index}. #{name}"
       end.join(" ")
     puts "The line is currently: #{line_order}"
   end
 end
 
-# assigns line number (index) at end of customer array
-# puts message telling new customer their place in line
+# take_a_number: call with deli array and new customer name
+# push new name to end of array
+# get index of new name; use new name & index (or array length) in 
+# puts "Welcome, #{name}, you are number #{index} in line."
 def take_a_number(array, name)
-
-
+  array.push(name)
+  puts "Welcome, #{name}, you are number #{array.length} in line."
+  array
 end
 
  # now_serving puts  next person in line and remove them from front of array.
